161e95de507637038668f5892a62816ed20fc35d
vernetto <Ernesto0z>
2023-11-19 15:50:59 +0100

    mocking OK

--- actualModule.test.js
+++ actualModule.test.js
@@ -1,12 +1,20 @@
+
 // actualModule.test.js
 const actualModule = require('./actualModule');
+jest.mock('./calledModule'); // This automatically mocks all exports from calledModule
 const calledModule = require('./calledModule');
 
 test('mocks the calledFunction and tests actualModule', () => {
-    // Mock the calledFunction inside the test
-    jest.spyOn(actualModule, 'actualFunction').mockReturnValue('Mocked Called Function Output');
-    jest.spyOn(calledModule, 'calledFunction').mockReturnValue('Mocked Called Function Output');
+    // Provide a mock implementation for calledFunction
+    calledModule.calledFunction.mockImplementation(() => 'Mocked Called Function Output');
 
     expect(actualModule.actualFunction()).toBe('Mocked Called Function Output');
-    expect(calledModule.calledFunction()).toBe('Mocked Called Function Output'); // This will pass
 });
+
+test('mocks the calledFunction and tests actualModule 2', () => {
+    // Provide a mock implementation for calledFunction
+    calledModule.calledFunction.mockImplementation(() => 'Mocked Called Function Output 2');
+
+    expect(actualModule.actualFunction()).toBe('Mocked Called Function Output 2');
+});
+
